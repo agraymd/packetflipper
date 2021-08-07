de78d8efa6ed0d0737ddef7015988eb68747ffc4
Alex <GrayAlexanderJ@gmail.com>
2021-08-06 22:01:51 -0400

    Changed style items, chnged wording of email signup

--- subnet_app/labs/static/labs/compare_text.js
+++ subnet_app/labs/static/labs/compare_text.js
@@ -3,11 +3,14 @@ document.getElementById("go_button").addEventListener("click", function(){
     var correct = document.getElementById("correct").innerHTML;
     var user_answer = document.getElementById("user_input").value;
     var user_answer_text_input = document.getElementById("user_input")
+    var input_instructions = document.getElementById("input_instructions")
+
     if (user_answer == correct){
        var correct_answer_textarea = document.getElementById("correct_answer_textarea");
        correct_answer_textarea.classList.remove("hidden");
        user_answer_text_input.classList.remove("error");
        user_answer_text_input.classList.add("correct");
+       input_instructions.classList.add("hidden");
     } else {
        user_answer_text_input.classList.add("error");
     }
