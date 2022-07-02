4a38e24da39f4bae20df1a0e8a164ed72b5488a0
agraymd <grayalexanderj@gmail.com>
2022-07-02 13:05:34 -0400

    fixed settings file to accept a list in .env file for ALLOWED_HOST, changed index to remove ads and email signup

--- subnet_app/subnet_app/settings.py
+++ subnet_app/subnet_app/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =  False
 
-ALLOWED_HOSTS =[ env('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
